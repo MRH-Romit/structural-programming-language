8b1683f7f8b07ef7a0005b5e21b12cc8ca3944df
Redwan Hossain Romit <mdromit2002@gmail.com>
2024-03-03 23:39:30 +0600

    something in the way

--- Nested Loop/One.c
+++ Nested Loop/One.c
@@ -7,12 +7,11 @@ int main()
     printf("Enter the number of rows for the pattern: ");
     scanf("%d", &rows);
 
-    // Outer loop for rows
     for (i = 1; i <= rows; i++)
     {
-        // Inner loop for printing the sequence "123"
+
         for (j = 1; j <= 3; j++)
-        { // You want to print "123", so loop from 1 to 3
+        {
             printf("%d", j);
         }
         printf("\n");
