c819e961f83907182d5b94f51a320cb01ac515b0
Redwan Hossain Romit <mdromit2002@gmail.com>
2024-03-03 23:36:48 +0600

    twelve added

---
+++ Loop/twelve.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, first = 1, second = 1, next;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series up to %d terms: ", n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d, ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}
