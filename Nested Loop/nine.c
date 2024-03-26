151dd561444bf6518d26f61f97cdf6a7c486920a
MRH-romit <mdromit2002@gmail.com>
2024-03-27 01:52:11 +0600

    added question of nested loop

---
+++ Nested Loop/nine.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+printf("Enter the number binary code do you want to add: \n");
+int n;
+scanf("%d", &n);
+
+for (int i = n; i >= 1; i--) {
+    for (int j = 1; j <= i; j++) {
+        
+        if (j%2==0){
+            printf("0 ");
+        }else{
+            printf("1 ");
+        }
+        
+    }
+} 
+
+
+
+
+    return 0;
+}
