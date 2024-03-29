ccc7fe49a5d4437d6070b67f938b4d1858c75075
MRH-romit <mdromit2002@gmail.com>
2024-03-29 23:41:05 +0600

    added 1d array problem eight solve

--- One D Array/eight.c
+++ One D Array/eight.c
@@ -1,9 +1,55 @@
-#include <stdio.h>'
-int main(){
+#include <stdio.h>
 
-int *array=  malloc()
+int main() {
 
+    int n;
+    printf("how many array elements do you want to add: ");
+    scanf("%d", &n);
 
+    // Check if the user entered a non-positive size
+    if (n <= 0) {
+        printf("Invalid size.\n");
+        return 1;
+    }
+
+    int arr[n];
+
+    // Insert elements
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Initialize max and min with the first element of the array
+    int max = arr[0], min = arr[0];
+    int max_index = 0, min_index = 0;
+
+    // Checking the max, min
+    for (int i = 1; i < n; i++) {
+
+        if (arr[i] > max) {
+            max = arr[i];
+            max_index = i;
+
+        }
+        if (arr[i] < min) { // Use if instead of else if for independent checks
+            min = arr[i];
+            min_index = i;
+        }
+    }
+
+    // Print out the elements
+    printf("Elements: ");
+    for (int j = 0; j < n; j++) {
+        printf("%d ", arr[j]);
+    }
+    printf("\n");
+
+    // Printing out all the things
+    printf("Max is: %d\n", max);
+    printf("Min is: %d\n", min);
+    printf("Max index is: %d\n", max_index+1);
+    printf("Min index is: %d\n", min_index+1);
 
     return 0;
 }
--- Two D Array/two.c
+++ Two D Array/two.c
@@ -21,9 +21,6 @@ for (i=0;i<n;i++){
                     printf("\n");
                 }
 
-
-
-
 return 0;
 }
 
